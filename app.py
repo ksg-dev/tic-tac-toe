b64918af91cdd337949ec1263f8b5a5ecd41a10e
ksg-dev <ksg.dev.data@gmail.com>
2024-09-18 12:22:36 -0500

    add session logic

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from tempfile import mkdtemp
 
 app = Flask(__name__)
 
+# Use session so each player can have a different board
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -12,10 +13,14 @@ Session(app)
 @app.route("/")
 def index():
 
+    # First, if no board in session, create a new board
     if "board" not in session:
+        # Create blank board
         session["board"] = [[None, None, None], [None, None, None], [None, None, None]]
+        # Also store whose turn it is in session, X starts
         session["turn"] = "X"
 
+    # Inside template, need to have access to board, and whose turn it is
     return render_template("game.html", game=session["board"], turn=session["turn"])
 
 @app.route("/play/<int:row>/<int:col>")
